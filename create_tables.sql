a6e3d7caa3eb2d6dd04d97d8cc7ba5011ff919e6
Hugo Henrique Fumero de Souza <hugohfsouza@hotmail.com>
2021-11-26 13:44:27 -0300

    estrutura do banco

--- create_tables.sql
+++ create_tables.sql
@@ -10,6 +10,17 @@ CREATE TABLE `repositorios` (
   `prs_analisados` tinyint(4) DEFAULT NULL,
   `qtdPrs` bigint(20) DEFAULT NULL,
   `linguagemReferencia` text,
+  `qtdArquivosStringTeste` bigint(9) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unico_projeto` (`name`,`nameWithOwner`,`databaseId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2433 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `startstop` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(100) DEFAULT NULL,
+  `continuar` tinyint(4) DEFAULT '1',
+  `continuarSearch` tinyint(4) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `ix_name` (`token`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
